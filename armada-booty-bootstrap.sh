ce29f32e4f47e96bc186d84db833dcb5f02d9175
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-07-28 19:20:43 -0400

    Launch The Fleet

--- armada-booty-bootstrap.sh
+++ armada-booty-bootstrap.sh
@@ -6,7 +6,7 @@ sudo yum -y install git
 sudo yum -y install gcc
 sudo curl -O https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py --user
-python3 -m pip install requests
-sudo -H pip3 install requests
-python3 -m pip install numpy
-sudo -H pip3 install numpy
+sudo yum -y install apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>THE ARMADA OF THE DAMNED SUCKS!!!!!!!</h1>" | sudo tee /var/www/html/index.html
