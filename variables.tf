3cc158ab0c4834e94a65b4d4efea16fbc550a456
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-07-28 07:39:28 -0400

    updates

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable AWS_SECRET_ACCESS_KEY {
 # Variables Edited & Pushed By The Armada Captain
 variable armada_captain_ipaddressv4 {
     type = string
-    default = 0.0.0.0/0
+    default = "0.0.0.0/0"
 }
 
 variable swine_count {
